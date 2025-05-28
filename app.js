dbd8e0d6139a4cb3af80e171ba887f40ad126031
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-05-28 10:52:31 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const videoRoutes = require('./routes/videos');
+const userRoutes = require('./routes/users');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.use('/api/videos', videoRoutes);
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => console.log("Server started on http://localhost:3000"));
