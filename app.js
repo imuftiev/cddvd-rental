98c2d0414b3630b5e0fde3a7630ddf91be2f142f
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-05-28 11:17:02 +0300

    CRUD media entity realised

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
-const videoRoutes = require('./routes/videos');
-const userRoutes = require('./routes/users');
+// const videoRoutes = require('./routes/videos');
+// const userRoutes = require('./routes/users');
+const mediaRoutes = require('./routes/media');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/media', mediaRoutes);
 
 app.set('view engine', 'ejs');
 
-app.use('/api/videos', videoRoutes);
-app.use('/api/users', userRoutes);
+// app.use('/api/videos', videoRoutes);
+// app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
     res.render('index');
