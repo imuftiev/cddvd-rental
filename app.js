bc6062529335ef6ff3d69d54e6ec448512a1653d
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-05-30 13:03:21 +0300

    routes for admin, rentals, clients added

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const expressLayouts = require('express-ejs-layouts');
 
 const indexRoutes = require('./routes/index');
 const mediaRoutes = require('./routes/media');
+const clientsRoutes = require('./routes/clients');
 const authRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin');
+const rentalRoutes = require('./routes/rentals');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,7 +31,10 @@ app.set('layout', 'layout');
 app.set('view engine', 'ejs');
 
 app.use('/media', mediaRoutes);
+app.use('/clients', clientsRoutes);
 app.use(authRoutes);
+app.use('/admin', adminRoutes);
+app.use('/rentals', rentalRoutes);
 app.use('/', indexRoutes);
 
 app.listen(3000, () => console.log("Server started on http://localhost:3000"));
