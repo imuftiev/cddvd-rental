d017937b9ba6697732072d906710d34a831d4dad
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-05-30 13:07:05 +0300

    authentification checks for distr and admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,6 +5,14 @@ function isAuthenticated(req, res, next) {
   res.redirect('/login');
 }
 
+function isAdminAndDistr(req, res, next) {
+  if (req.session.user && req.session.user.role === 'admin' || req.session.user && req.session.user.role === 'distr') {
+    return next();
+  }
+  res.status(403).send('Доступ запрещен (только для админа и дистрибьютора)');
+}
+
+
 function isAdmin(req, res, next) {
   if (req.session.user && req.session.user.role === 'admin') {
     return next();
@@ -12,7 +20,14 @@ function isAdmin(req, res, next) {
   res.status(403).send('Доступ запрещен (только для админа)');
 }
 
+function isDistributor(req, res, next) {
+  if (req.session.user?.role === 'distr') return next();
+  res.status(403).send('Доступ запрещен (только для дистрибьютора)');
+}
+
 module.exports = {
   isAuthenticated,
-  isAdmin
+  isAdmin,
+  isDistributor,
+  isAdminAndDistr
 };
